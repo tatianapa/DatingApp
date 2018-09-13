99392de51005328e1bd3bfd6dc1b3a3c7f69ad5b
tatianapa <tatiana.pavlov1991@gmail.com>
2018-09-13 14:20:03 -0700

    Client side logon and registration added

--- DatingApp.API/Controllers/ValuesController.cs
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -20,7 +20,7 @@ namespace DatingApp.Controllers
            _context = context;
 
         }
-        // GET api/values
+        [AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult> GetValues()
         {
