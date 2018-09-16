73fc20f3045fd74680de7cef412be7d3572a6faf
tatianapa <tatiana.pavlov1991@gmail.com>
2018-09-16 10:58:24 -0700

    End of section 5 - Error handling

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -20,7 +20,7 @@ export class NavComponent implements OnInit {
     this.authService.login(this.model).subscribe(next => {
       console.log("logged in succesfully");
     }, error =>{
-      console.log("failed to login");
+      console.log(error);
     });
   }
 
