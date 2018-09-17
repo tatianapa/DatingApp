82f6becb65671563040b9550ef53afbe53a57406
tatianapa <tatiana.pavlov1991@gmail.com>
2018-09-17 14:19:43 -0700

    End of Section 6 - working on ux on client side

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
 
 
 @Component({
@@ -11,29 +12,27 @@ export class NavComponent implements OnInit {
 
   model: any = {};
 
-  constructor(private authService: AuthService) { }
+  constructor(public authService: AuthService, private alertify: AlertifyService) { }
 
   ngOnInit() {
   }
 
   login(){
     this.authService.login(this.model).subscribe(next => {
-      console.log("logged in succesfully");
+      this.alertify.success("logged in succesfully");
     }, error =>{
-      console.log(error);
+      this.alertify.error(error);
     });
   }
 
   loggedIn(){
-    const token = localStorage.getItem('token');
-
-    return !!token;
+    return this.authService.loggedIn();
   }
 
   logout(){
     localStorage.removeItem('token');
 
-    console.log("logged out");
+    this.alertify.message("logged out");
   }
 
 }
--- DatingApp-SPA/src/app/register/register.component.ts
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
 
 
 @Component({
@@ -14,7 +15,7 @@ export class RegisterComponent implements OnInit {
 
   model: any = {};
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private alertify: AlertifyService) { }
 
   ngOnInit() {
   }
@@ -22,16 +23,16 @@ export class RegisterComponent implements OnInit {
   register() {
    this.authService.register(this.model).subscribe(
      () => {
-       console.log("registration successfull")},
+       this.alertify.success("registration successfull")},
      error => {
-       console.log(error);
+      this.alertify.error(error);
      }
    );
   }
 
   cancel(){
     this.cancelRegister.emit(false);
-   console.log('cancelled');
+   
   }
 
 }
